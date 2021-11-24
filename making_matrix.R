694ec43ebe06c3899f6b954aa28f008cda3b2559
Jackie Ouzman <jackie.ouzman@csiro.au>
2021-11-24 18:15:55 +1030

    mess aground with options

--- making_matrix.R
+++ making_matrix.R
@@ -59,9 +59,6 @@ martix_2 <- read.csv("V:/Marlborough regional/climate/jax_working/matrix2.csv")
 martix_3 <- read.csv("V:/Marlborough regional/climate/jax_working/matrix3.csv")
 
 
-
-
-
 matrix_1_long <-
   martix_1 %>%
   pivot_longer(
@@ -93,5 +90,227 @@ matrix_3_long <-
 coord <- left_join(matrix_1_long, matrix_2_long)
 coord_value <- left_join(coord, matrix_3_long)
 coord_value
+
+write.csv(coord_value, "V:/Marlborough regional/climate/jax_working/coord_value.csv")
+
+## it does not look quite right ? I am not sure about this flip comment?
+
+##################################################################################################################################
+
+
+
+martix_1 <- read.csv("V:/Marlborough regional/climate/jax_working/matrix1_raw.csv")
+martix_2 <- read.csv("V:/Marlborough regional/climate/jax_working/matrix2_raw.csv")
+martix_3 <- read.csv("V:/Marlborough regional/climate/jax_working/matrix3_raw.csv")
+
+names(martix_2)
+
+martix_2 <- martix_2 %>% dplyr::select(-"X")
+
+str(martix_1)
+
+matrix_1_long <-
+  martix_1 %>%
+  pivot_longer(
+    cols = starts_with("V"),
+    names_to = "clm_ID",
+    values_to = "X"
+  )
+
+
+matrix_2_long <-
+  martix_2 %>%
+  pivot_longer(
+    cols = starts_with("V"),
+    names_to = "clm_ID",
+    values_to = "Y"
+  )
+
+str(martix_3)
+matrix_3_long <-
+  martix_3 %>%
+  pivot_longer(
+    cols = starts_with("V"),
+    names_to = "clm_ID",
+    values_to = "value"
+  )
+
+
+# join them togther
+
+coord <- cbind(matrix_1_long, matrix_2_long)
+coord_value <- cbind(coord, matrix_3_long)
+names(coord_value)
+
+coord_value <- coord_value %>% dplyr::select("X", "Y", "value")
+
+coord_value <- coord_value %>% mutate(id = row_number())
+
+write.csv(coord_value, "V:/Marlborough regional/climate/jax_working/coord_value_raw.csv")
+### still not right!!!####
+
+
+
+##################################################################################################################################
+
+
+
+martix_1 <- read.csv("V:/Marlborough regional/climate/jax_working/matrix1_raw.csv")
+martix_2 <- read.csv("V:/Marlborough regional/climate/jax_working/matrix2_raw.csv")
+martix_3 <- read.csv("V:/Marlborough regional/climate/jax_working/matrix3_flipV.csv")
+
+names(martix_2)
+
+martix_2 <- martix_2 %>% dplyr::select(-"X")
+
+str(martix_1)
+
+matrix_1_long <-
+  martix_1 %>%
+  pivot_longer(
+    cols = starts_with("V"),
+    names_to = "clm_ID",
+    values_to = "X"
+  )
+
+
+matrix_2_long <-
+  martix_2 %>%
+  pivot_longer(
+    cols = starts_with("V"),
+    names_to = "clm_ID",
+    values_to = "Y"
+  )
+
+str(martix_3)
+matrix_3_long <-
+  martix_3 %>%
+  pivot_longer(
+    cols = starts_with("V"),
+    names_to = "clm_ID",
+    values_to = "value"
+  )
+
+
+# join them togther
+
+coord <- cbind(matrix_1_long, matrix_2_long)
+coord_value <- cbind(coord, matrix_3_long)
+names(coord_value)
+
+coord_value <- coord_value %>% dplyr::select("X", "Y", "value")
+
+coord_value <- coord_value %>% mutate(id = row_number())
+
+write.csv(coord_value, "V:/Marlborough regional/climate/jax_working/coord_value_Vflip.csv")
+
+
+##################################################################################################################################
+
+### Flip Horizontal ###
+
+martix_1 <- read.csv("V:/Marlborough regional/climate/jax_working/matrix1_Flip_H.csv")
+martix_2 <- read.csv("V:/Marlborough regional/climate/jax_working/matrix2_Flip_H.csv")
+martix_3 <- read.csv("V:/Marlborough regional/climate/jax_working/matrix3_Flip_H.csv")
+
+names(martix_2)
+
+martix_2 <- martix_2 %>% dplyr::select(-"X")
+
+str(martix_1)
+
+matrix_1_long <-
+  martix_1 %>%
+  pivot_longer(
+    cols = starts_with("V"),
+    names_to = "clm_ID",
+    values_to = "X"
+  )
+
+
+matrix_2_long <-
+  martix_2 %>%
+  pivot_longer(
+    cols = starts_with("V"),
+    names_to = "clm_ID",
+    values_to = "Y"
+  )
+
+str(martix_3)
+matrix_3_long <-
+  martix_3 %>%
+  pivot_longer(
+    cols = starts_with("V"),
+    names_to = "clm_ID",
+    values_to = "value"
+  )
+
+
+# join them togther
+
+coord <- cbind(matrix_1_long, matrix_2_long)
+coord_value <- cbind(coord, matrix_3_long)
+names(coord_value)
+
+coord_value <- coord_value %>% dplyr::select("X", "Y", "value")
+
+coord_value <- coord_value %>% mutate(id = row_number())
+
+write.csv(coord_value, "V:/Marlborough regional/climate/jax_working/coord_value_Hflip.csv")
+
+
+
+##################################################################################################################################
+
+### Flip V and Horizontal again ###
+
+martix_1 <- read.csv("V:/Marlborough regional/climate/jax_working/matrix1_Flip_V_H.csv")
+martix_2 <- read.csv("V:/Marlborough regional/climate/jax_working/matrix2_Flip_V_H.csv")
+martix_3 <- read.csv("V:/Marlborough regional/climate/jax_working/matrix3_Flip_V_H.csv")
+
+names(martix_2)
+
+#martix_2 <- martix_2 %>% dplyr::select(-"X")
+
+str(martix_1)
+
+matrix_1_long <-
+  martix_1 %>%
+  pivot_longer(
+    cols = starts_with("X"),
+    names_to = "clm_ID",
+    values_to = "X"
+  )
+
+
+matrix_2_long <-
+  martix_2 %>%
+  pivot_longer(
+    cols = starts_with("X"),
+    names_to = "clm_ID",
+    values_to = "Y"
+  )
+
+str(martix_3)
+matrix_3_long <-
+  martix_3 %>%
+  pivot_longer(
+    cols = starts_with("X"),
+    names_to = "clm_ID",
+    values_to = "value"
+  )
+
+
+# join them togther
+
+coord <- cbind(matrix_1_long, matrix_2_long)
+coord_value <- cbind(coord, matrix_3_long)
+names(coord_value)
+
+coord_value <- coord_value %>% dplyr::select("X", "Y", "value")
+
+coord_value <- coord_value %>% mutate(id = row_number())
+
+write.csv(coord_value, "V:/Marlborough regional/climate/jax_working/coord_value_V_Hflip.csv")
 
 
