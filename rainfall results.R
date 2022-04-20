8c60a71d570f8f0266c9908d387a91121422e53b
JackieOuzman <jackie.ouzman@csiro.au>
2022-04-20 16:07:22 +1000

    and some other climate info

--- rainfall results.R
+++ rainfall results.R
@@ -117,14 +117,27 @@ number_clusters %>%
   
 str(climate)
 
-climate %>% 
-  ggplot(aes(x = X , y = Y, colour = factor(K_Cluster_2)))+
-  geom_point()
 
+#climate_cluster <- read.csv("V:/Viticulture/Marlborough regional/climate/climate_data_2022_vineyards_R/Climate_data_as_pts/GS_rainfall_kmean_2.csv")
+#climate_cluster <- read.csv("V:/Viticulture/Marlborough regional/climate/climate_data_2022_vineyards_R/Climate_data_as_pts/GST_kmean.csv")
+#climate_cluster <- read.csv("V:/Viticulture/Marlborough regional/climate/climate_data_2022_vineyards_R/Climate_data_as_pts/GDD_kmean.csv")
+#climate_cluster <- read.csv("V:/Viticulture/Marlborough regional/climate/climate_data_2022_vineyards_R/Climate_data_as_pts/DOH_kmean.csv")
+#climate_cluster <- read.csv("V:/Viticulture/Marlborough regional/climate/climate_data_2022_vineyards_R/Climate_data_as_pts/DOF_kmean.csv")
+climate_cluster <- read.csv("V:/Viticulture/Marlborough regional/climate/climate_data_2022_vineyards_R/Climate_data_as_pts/DOV_kmean.csv")
+names(climate_cluster)
 
-
-
-
-
+climate_cluster %>% 
+  ggplot(aes(x = X , y = Y, colour = factor(k_cluster)))+
+  geom_point()+
+  theme_bw()+
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        legend.position = "none"
+        #legend.title = element_blank()
+        )
 
 
