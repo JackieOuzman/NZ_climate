1800c5c761ddf3ed3c77c37a8ca217b89d224d64
Jackie Ouzman <jackie.ouzman@csiro.au>
2022-05-03 16:33:34 +0930

    correlation with modlled data

--- Model_Climate_explore.R
+++ Model_Climate_explore.R
@@ -22,5 +22,29 @@ climate_grid_values <- climate_grid_values %>%
 names(climate_grid_values)
 
 ### needs to be long format
-climate_grid_values_narrow <- climate_grid_values
-pivot_longer()
+climate_grid_values_narrow <- climate_grid_values %>% 
+pivot_longer(cols = DOV_2013:DOH_2021,
+             names_to = "variable_year",
+             values_to = "value"
+)
+
+head(climate_grid_values_narrow)
+
+
+climate_grid_values_narrow <- climate_grid_values_narrow %>% 
+  separate(variable_year, c("variable", "year"), sep = "_" )
+head(climate_grid_values_narrow)
+
+
+## I need to make it wide again? so I have clm for year and then a clm for each variable
+climate_grid_values_wide <- climate_grid_values_narrow %>% 
+  pivot_wider(names_from = variable,
+              values_from = value)
+
+head(climate_grid_values_wide)
+
+climate_grid_values_wide %>% 
+  ggplot(aes(DOF, DOH))+
+  geom_point()+
+  facet_wrap(.~ year)
+  
