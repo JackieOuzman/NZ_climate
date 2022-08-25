baac295ed4d749b71484ba572631541f587f98f5
JackieOuzman <jackie.ouzman@csiro.au>
2022-08-25 15:48:21 +1000

    still to check results but neater!

--- step2_cluster_prep_melb_computer_generic.R
+++ step2_cluster_prep_melb_computer_generic.R
@@ -29,57 +29,34 @@ plot(grid_pts_vec)
 
 
 
-
 ## list in the climate data
-list.files(paste0(path, "Marlborough regional/climate/climate_data_2022_vineyards_R/"))#Melb   computer
+file_list <- list.files(paste0(path, "Marlborough regional/climate/climate_data_2022_vineyards_R_august2022/"),  pattern = ".tif")#Melb   computer
 
-#list.files("V:/Marlborough regional/climate/climate_data_2022_vineyards_R/") #Jackies computer
 
-climate_type <-"DOH"     #"rain""GST""GDD" "DOH"DOF""DOV"
+### ---- TEST ---####
+#file_list <-file_list[63]
+### ---- END OF TEST ---####
 
-# climate_files <- list.files("V:/Viticulture/Marlborough regional/climate/climate_data_2022_vineyards_R/",
-#            pattern = climate_type)
-climate_files <- list.files(paste0(path, "Marlborough regional/climate/climate_data_2022_vineyards_R/"),
-                            pattern = climate_type)
-
-#View(climate_files)
-
-climate_files <- as.data.frame(climate_files)
-
-climate_tiff <- climate_files %>% 
-  dplyr::filter(str_detect(climate_files,".tiff")) %>% 
-  dplyr::filter(str_detect(climate_files,"aux.xml", negate = TRUE))
-
-climate_tiff$climate_files <- as.character(climate_tiff$climate_files)
-#Turn into a list
-
-file_list <- climate_tiff[["climate_files"]] 
-
-file_list
-
-#file_list <- file_list[1]
+# file_list_GDD <- list.files(paste0(path, "Marlborough regional/climate/climate_data_2022_vineyards_R_august2022/"),  pattern = "GDD")
+# file_list_GDD [1]
+# file_list <- file_list_GDD
 
 ## this will be the loop
 for (file_list in file_list){
 
   
-## get the year from the file name
-  #step1<-sub(".tiff*", "", file_list) # Extract characters before pattern # for the rainfall files AND doh
-  step1<-sub(".tiff*", "", file_list) # Extract characters before pattern # for GST
+climate_type <- stringr ::str_sub(file_list,1,3) # Extract characters before pattern GST and GDD 
+climate_type
   
-  
-  
-  bit_to_extract <- paste0(".*",climate_type,"_proj_resample_mask_" )
-  year2 <-sub(bit_to_extract, "", step1)# Extract characters after pattern
-  #year <- str_sub(year2,-4,-1)
-  year <- str_sub(year2,4,8) #for DOH
-  rm(step1, year2, bit_to_extract)
+year<-stringr ::str_sub(file_list,28,31)
+year
+ 
+
 
 ## bring in the climate raster 
-  # climate <- rast(paste0("V:/Viticulture/Marlborough regional/climate/climate_data_2022_vineyards_R/",
-  #                        file_list)) #melb computer
-  climate <- rast(paste0(path,"Marlborough regional/climate/climate_data_2022_vineyards_R/",
-                         file_list)) #my computer
+
+climate <- rast(paste0(path,"Marlborough regional/climate/climate_data_2022_vineyards_R_august2022/",
+                         file_list)) #melb computer
 
   
   ## extract the points for the cliamte raster
@@ -107,71 +84,71 @@ rm(climate, name,year)
 
 } 
 
-# climate_all_rain <- rbind(
-#   rain_2013,
-#   rain_2014,
-#   rain_2015,
-#   rain_2016,
-#   rain_2017,
-#   rain_2018,
-#   rain_2019,
-#   rain_2020,
-#   rain_2021
-#   )
-# 
-# rm(rain_2013,
-#    rain_2014,
-#    rain_2015,
-#    rain_2016,
-#    rain_2017,
-#    rain_2018,
-#    rain_2019,
-#    rain_2020,
-#    rain_2021)
+climate_all_rain <- rbind(
+  rai_2013,
+  rai_2014,
+  rai_2015,
+  rai_2016,
+  rai_2017,
+  rai_2018,
+  rai_2019,
+  rai_2020,
+  rai_2021
+  )
 
-# climate_all_GST <- rbind(
-#   GST_2013,
-#   GST_2014,
-#   GST_2015,
-#   GST_2016,
-#   GST_2017,
-#   GST_2018,
-#   GST_2019,
-#   GST_2020,
-#   GST_2021
-#   )
-# 
-# rm(GST_2013,
-# GST_2014,
-# GST_2015,
-# GST_2016,
-# GST_2017,
-# GST_2018,
-# GST_2019,
-# GST_2020,
-# GST_2021)
+rm(rai_2013,
+   rai_2014,
+   rai_2015,
+   rai_2016,
+   rai_2017,
+   rai_2018,
+   rai_2019,
+   rai_2020,
+   rai_2021)
 
-# climate_all_GDD <- rbind(
-#   GDD_2013,
-#   GDD_2014,
-#   GDD_2015,
-#   GDD_2016,
-#   GDD_2017,
-#   GDD_2018,
-#   GDD_2019,
-#   GDD_2020,
-#   GDD_2021
-#   )
-# 
-# rm(GDD_2013,
-# GDD_2014,
-# GDD_2015,
-# GDD_2016,
-# GDD_2017,
-# GDD_2018,
-# GDD_2019,
-# GDD_2020,
-# GDD_2021)
+climate_all_GST <- rbind(
+  GST_2013,
+  GST_2014,
+  GST_2015,
+  GST_2016,
+  GST_2017,
+  GST_2018,
+  GST_2019,
+  GST_2020,
+  GST_2021
+  )
+
+rm(GST_2013,
+GST_2014,
+GST_2015,
+GST_2016,
+GST_2017,
+GST_2018,
+GST_2019,
+GST_2020,
+GST_2021)
+
+climate_all_GDD <- rbind(
+  GDD_2013,
+  GDD_2014,
+  GDD_2015,
+  GDD_2016,
+  GDD_2017,
+  GDD_2018,
+  GDD_2019,
+  GDD_2020,
+  GDD_2021
+  )
+
+rm(GDD_2013,
+GDD_2014,
+GDD_2015,
+GDD_2016,
+GDD_2017,
+GDD_2018,
+GDD_2019,
+GDD_2020,
+GDD_2021)
 
 climate_all_DOH <- rbind(
   DOH_2013,
@@ -196,61 +173,61 @@ rm(DOH_2013,
    DOH_2021)
 
 
-# climate_all_DOF <- rbind(
-#   DOF_2013,
-#   DOF_2014,
-#   DOF_2015,
-#   DOF_2016,
-#   DOF_2017,
-#   DOF_2018,
-#   DOF_2019,
-#   DOF_2020,
-#   DOF_2021
-# )
-# 
-# rm(DOF_2013,
-#    DOF_2014,
-#    DOF_2015,
-#    DOF_2016,
-#    DOF_2017,
-#    DOF_2018,
-#    DOF_2019,
-#    DOF_2020,
-#    DOF_2021)
+climate_all_DOF <- rbind(
+  DOF_2013,
+  DOF_2014,
+  DOF_2015,
+  DOF_2016,
+  DOF_2017,
+  DOF_2018,
+  DOF_2019,
+  DOF_2020,
+  DOF_2021
+)
 
-# climate_all_DOV <- rbind(
-#   DOV_2013,
-#   DOV_2014,
-#   DOV_2015,
-#   DOV_2016,
-#   DOV_2017,
-#   DOV_2018,
-#   DOV_2019,
-#   DOV_2020,
-#   DOV_2021
-# )
-# 
-# rm(DOV_2013,
-#    DOV_2014,
-#    DOV_2015,
-#    DOV_2016,
-#    DOV_2017,
-#    DOV_2018,
-#    DOV_2019,
-#    DOV_2020,
-#    DOV_2021)
+rm(DOF_2013,
+   DOF_2014,
+   DOF_2015,
+   DOF_2016,
+   DOF_2017,
+   DOF_2018,
+   DOF_2019,
+   DOF_2020,
+   DOF_2021)
+
+climate_all_DOV <- rbind(
+  DOV_2013,
+  DOV_2014,
+  DOV_2015,
+  DOV_2016,
+  DOV_2017,
+  DOV_2018,
+  DOV_2019,
+  DOV_2020,
+  DOV_2021
+)
+
+rm( DOV_2013,
+    DOV_2014,
+    DOV_2015,
+    DOV_2016,
+    DOV_2017,
+    DOV_2018,
+    DOV_2019,
+    DOV_2020,
+    DOV_2021)
 
 #########################################################################################
 
-
+names(climate_all_rain)
 
 
 ### make dataset wider for each climate variable
 climate_all_rain_wider <- climate_all_rain %>% 
   pivot_wider(
     names_from = year,
-    names_prefix = "rain_",
-    values_from = rain)
+    names_prefix = "rai_",
+    values_from = rai)
 
 climate_all_GST_wider <- climate_all_GST %>% 
   pivot_wider(
@@ -306,10 +283,13 @@ climate_all <- left_join(climate_all,
 
 names(climate_all)
 
+
+
 write.csv(
   climate_all,
   paste0(
-    "V:/Marlborough regional/climate/climate_data_2022_vineyards_R/Climate_data_as_pts/",
+    path,
+    "Marlborough regional/climate/climate_data_2022_vineyards_R_august2022/Climate_data_as_pts_august2022/Climate_data_as_pts_august2022",
     "climate_all_cluster_input.csv"),
           row.names = FALSE) 
 
